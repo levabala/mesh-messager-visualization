eaf5dda812779a2b1d6df66d8c2d60f1b59b6b45
levabala <levabala@ya.ru>
2020-03-30 15:14:08 +0300

    it works!

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import './index.scss';
 
+import { addNode, communicationLocal, connectedNodes } from 'mesh-messager-local';
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Node } from '../../mesh-messager-core/build';
 import { register } from './serviceWorker';
 import TestComponent from './TestComponent';
+
+const rootNode = new Node(communicationLocal).setLogging(false);
+addNode(rootNode);
+console.log(connectedNodes, rootNode);
 
 // here we disable console and performance for better production experience
 // console.log(process.env.NODE_ENV);
